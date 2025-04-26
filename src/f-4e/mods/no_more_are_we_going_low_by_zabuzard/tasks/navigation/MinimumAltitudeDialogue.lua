ad5dbbf5523d86843c690a3a724256a8c49695e7
Zabuzard <zabuza.dev@gmail.com>
2025-04-26 12:36:07 +0200

    Added "no more are we going low" mod

---
+++ src/f-4e/mods/no_more_are_we_going_low_by_zabuzard/tasks/navigation/MinimumAltitudeDialogue.lua
@@ -0,0 +1,9 @@
+local Class = require('base.Class')
+local Task = require('base.Task')
+
+function MinimumAltitudeDialogue:Constructor()
+  -- do nothing, lol
+end
+
+MinimumAltitudeDialogue:Seal()
+return MinimumAltitudeDialogue
